05efddee8fd3d11c53365a6ba72f0063d67c46f0
dharmendrasinha <59174574+dharmendrasinha@users.noreply.github.com>
2020-01-04 20:53:37 -0500

    UI changes for pool and commitment

--- src/main/webapp/app/admin/projects/projectactivity.component.ts
+++ src/main/webapp/app/admin/projects/projectactivity.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProjectsService } from './projects.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'jhi-projectactivity',
@@ -7,14 +12,67 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProjectActivityComponent implements OnInit {
   projectNo: any;
-  constructor(private activatedRoute: ActivatedRoute) {
+  commitmentForm: FormGroup;
+  commitmentInfo: any;
+  drawdownHistoryList: any;
+  dataSourceDrawdowns: any;
+  commitmentsScheduleList: any;
+  dataSourceCommitmentsSchedule: any;
+
+  @ViewChild('sort1', { static: true }) sort1: MatSort;
+  @ViewChild('sort2', { static: true }) sort2: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  displayedColumnsDrawdowns: string[] = [
+    'date',
+    'note',
+    'status',
+    'amount',
+    'term',
+    'rate',
+    'maturityDate',
+    'intSubsidyAl',
+    'intSubsidyCr'
+  ];
+
+  displayedColumnsCommitmentsSchedule: string[] = ['expDate', 'amtExp', 'proReAvailability'];
+
+  constructor(private activatedRoute: ActivatedRoute, private fb: FormBuilder, private projectsService: ProjectsService) {
     this.activatedRoute.paramMap.subscribe(params => {
       this.projectNo = +params.get('projectNo');
-      if (this.projectNo) {
-        // eslint-disable-next-line no-console
-      }
+    });
+
+    this.projectsService.getCommitmentsByProject(this.projectNo).subscribe((res: any) => {
+      this.commitmentInfo = res;
+      this.commitmentForm.patchValue({
+        commitmentStatus: this.commitmentInfo.commitmentStatus,
+        commitmentBal: this.commitmentInfo.commitmentBal,
+        commitmentExpiration: this.commitmentInfo.commitmentExpiration,
+        commenceDate: this.commitmentInfo.commenceDate,
+        avaiableBal: this.commitmentInfo.avaiableBal,
+        totalDrawdowns: this.commitmentInfo.totalDrawdowns,
+        recentDrawdown: this.commitmentInfo.recentDrawdown
+      });
+    });
+
+    this.projectsService.getDrawdownHistoriesByProject(this.projectNo).subscribe((res: any) => {
+      this.drawdownHistoryList = res;
+      this.dataSourceDrawdowns = new MatTableDataSource(this.drawdownHistoryList);
+      this.dataSourceDrawdowns.paginator = this.paginator;
+      this.dataSourceDrawdowns.sort = this.sort2;
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.commitmentForm = this.fb.group({
+      id: [{ value: null }],
+      commitmentStatus: [{ value: null, disabled: true }],
+      commitmentBal: [{ value: null, disabled: true }],
+      commitmentExpiration: [{ value: null, disabled: true }],
+      commenceDate: [{ value: null, disabled: true }],
+      avaiableBal: [{ value: null, disabled: true }],
+      totalDrawdowns: [{ value: null, disabled: true }],
+      recentDrawdown: [{ value: null, disabled: true }]
+    });
+  }
 }
